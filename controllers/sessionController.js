2957ead3f6e5ef50b649ed2e05fd69c19233ee2b
Davit <davit.hambardzumyan.est@gmail.com>
2026-01-12 14:47:48 +0400

    Add Accept

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1772,7 +1772,7 @@ async function acceptGoogleCookies(page) {
     // Common "Accept all" translations in various languages
     const acceptButtonTexts = [
         // English
-        'Accept all', 'Accept all cookies', 'Accept all settings',
+        'Accept all', 'Accept all cookies', 'Accept all settings', 'Accept',
         // French
         'Tout accepter', 'Tout accepter et continuer',
         // German
