23cee154b6e9c98672003feeb674e5d3eb43002d
Davit <davit.hambardzumyan.est@gmail.com>
2026-02-09 18:13:22 +0400

    fix config

--- extensions/2captcha/common/config.js
+++ extensions/2captcha/common/config.js
@@ -2,7 +2,7 @@ var Config = {
 
     default: {
         isPluginEnabled: true,
-        apiKey: null,
+        apiKey: "74b59f2ae349e4fec26b963c371180c8",
         valute: "USD",
         email: null,
         autoSubmitForms: true,
@@ -39,9 +39,9 @@ var Config = {
         repeatOnErrorTimes: 0,
         repeatOnErrorDelay: 0,
         buttonPosition: 'inner',
-        useProxy: true,
+        useProxy: "31yF7wX76M8lLndv",
         proxytype: "HTTP",
-        proxy: "",
+        proxy: "package-314570-country-us-sessionid-uv2to6z9OBv8f5WR_x8-sessionlength-1200-opt-wb:31yF7wX76M8lLndv@proxy.soax.com:5000",
         blackListDomain: "example.com\n2captcha.com/auth\nrucaptcha.com/auth",
         normalSources: [],
         autoSubmitRules: [{
